164cb3f1c4450eb948b88477436ea99a35f12f72
Linnéa Toth <linnea@liroit.com>
2025-09-02 10:15:02 +0200

    data deleted after clarification from instructor

--- src/data/items.ts
+++
@@ -1,9 +0,0 @@
-import type { Object } from "../types/types.ts";
-
-const items: Object[] = Array(20);
-items.map((o, i) => {
-  o.id = i;
-  o.itemName = `Item number ${i}`;
-});
-
-export { items };

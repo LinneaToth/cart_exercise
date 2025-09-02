af5f34bfb644e715d9dfcb10317226d0c257d9dc
Tom Larsson <tmlsn@hotmail.com>
2025-09-02 11:43:41 +0200

    dsd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import { useState } from "react";
-import type { Item } from "./types/types.ts";
+import { useReducer } from 'react';
+import type { Item } from './types/types.ts';
+
+const initialCart: Item[] = [];
 
 function App() {
+  const [cart, dispatch] = useReducer(cartReducer, initialCart);
+
+  function cartReducer(state, action) {
+    switch (action.type) {
+      case 'add':
+        return;
+
+      case 'toggle':
+        return;
+
+      case 'remove':
+        return;
+
+      case 'clear':
+        console.log('sdadsds');
+        return [...initialCart];
+
+      default:
+        throw new Error('Wrong!');
+    }
+  }
+
   return (
     <>
+      <button onClick={() => dispatch({ type: 'clear' })}>Delete</button>
       <ul></ul>
     </>
   );
